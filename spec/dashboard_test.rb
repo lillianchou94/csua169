7d6de510a494c546bb79b95f991715c9e3eb46ff
Lillian Chou <lillianchou94@gmail.com>
2016-03-11 06:33:13 +0000

    fixed test/controller/elections_controller_test

--- spec/dashboard_test.rb
+++ spec/dashboard_test.rb
@@ -1,4 +1,4 @@
-require 'app/controllers/elections_controller.rb'
+#require 'app/controllers/elections_controller.rb'
 require 'spec/spec_helper.rb'
 
 describe 'Ruby intro part 1' do
@@ -8,4 +8,15 @@ describe 'Ruby intro part 1' do
       get "/elections/show_elections"
     end
   end
+  
+  
+  describe "show_elections" do
+    it "calls the controller method that renders the show_elections view" do
+      expect(ElectionsController).to receive(:show_elections)
+      get "/elections/show_elections"
+    end
+  end
+  
+  
+  
 end
